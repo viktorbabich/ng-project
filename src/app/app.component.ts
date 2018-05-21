bb697cb75702df9a1bc16b3cc267d6b5b11874ba
Виктор Бабич <v.babich@artsofte.ru>
2018-05-21 10:58:40 +0500

    start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Model } from "./model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "todo-app",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
-  title = 'app works!';
+  model = new Model();
+  getName() {
+    return this.model.user;
+  }
 }
